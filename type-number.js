e37f0b4dd64fe8178b17f9f5a5fe985426e6e0e5
Bruno <gitbrunos@gmail.com>
2022-01-13 15:35:38 -0300

    primeiras aulas

---
+++ type-number.js
@@ -0,0 +1,15 @@
+ //Tipo Number
+ const a = 3
+ console.log(a)
+
+ //Ponto flutuante
+
+ const numFlutuante = 3.3
+ console.log(numFlutuante)
+
+ //NaN > not a number
+ const  alura = "curso da alura"
+ res = alura * a
+ console.log(res) 
+
+ 
