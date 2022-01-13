8dc2a62fab1177a13eaf5c2246447e81146b88a3
Bruno <gitbrunos@gmail.com>
2022-01-13 16:13:24 -0300

    dados

---
+++ type-boolean.js
@@ -0,0 +1,6 @@
+//Exemplos de booleanos 
+const a = 1
+var b = 1
+console.log(a === b)
+var b = 2
+console.log(a === b)
---
+++ variaveis.js
@@ -0,0 +1,4 @@
+var a = ("primeiro tipo de variável")
+let b = ("segundo tipo de variável")
+const c = ("terceiro tipo de variável")
+console.log(a + b + c)
