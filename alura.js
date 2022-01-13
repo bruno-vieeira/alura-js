d54dcdd8ce3fdddd4b0b96125c404e96b863baad
Bruno <gitbrunos@gmail.com>
2022-01-13 15:21:31 -0300

    tipos de dados js

--- alura.js
+++ alura.js
@@ -1,9 +1,29 @@
-a = []
-b = 20
-c = true 
-d = ''
-if (a == 0) {
-    console.log('Verdade')
-} else {
-    console.log('Falso')
-}
+ //Tipo Number
+ const a = 3
+ console.log(a)
+
+ //Ponto flutuante
+
+ const numFlutuante = 3.3
+ console.log(numFlutuante)
+
+ //NaN > not a number
+ const  alura = "curso da alura"
+ res = alura * a
+ console.log(res) 
+ 
+ //Tipo String + concatenação
+ const stra = "concatenando "
+ const strb = "strings"
+ console.log(stra + strb)
+ 
+ //Códigos convertidos 
+ const cifrao = '\u0024'
+ const aMaiusculo = '\u0041'
+ const tique = '\u2705'
+ const hiragana = '\u3041'
+
+ console.log(cifrao)
+ console.log(aMaiusculo)
+ console.log(tique)
+ console.log(hiragana)
